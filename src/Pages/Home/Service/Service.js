4954850597294e59471456efab00169703a30f11
mss38 <mss38k@gmail.com>
2022-04-17 21:37:38 +0600

    create service pages

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,106 @@
-import React from 'react';
+import React from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import pic1 from "../../../images/gallery/high-img (1).jpg";
+import pic4 from "../../../images/gallery/high-img (4).jpg";
+import pic5 from "../../../images/gallery/high-img (5).jpg";
+import pic6 from "../../../images/gallery/high-img (6).jpg";
+import pic7 from "../../../images/gallery/high-img (7).jpg";
+import pic8 from "../../../images/gallery/high-img (8).jpg";
+import pic9 from "../../../images/gallery/high-img (9).jpg";
+import pic10 from "../../../images/gallery/high-img (10).jpg";
+import pic11 from "../../../images/gallery/high-img (11).jpg";
+import pic12 from "../../../images/gallery/high-img (12).jpg";
 
 const Service = () => {
-   return (
-      <div>
-         <h2>This is Service</h2>
+  return (
+    <div>
+      <h2 className="text-center fw-bold text-info pb-4 pt-5 ">Welcome To Gallery</h2>
+      <div className="container">
+        <Row xs={1} md={4} className="g-4">
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic1} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic4} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic5} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic6} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic7} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic8} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic9} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic10} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic11} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={pic12} />
+              <Card.Footer>
+                <Button variant="primary">CheckOut</Button>{" "}
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Service;
